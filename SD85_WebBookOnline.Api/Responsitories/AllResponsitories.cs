f1c7b2fe804fd7a41b4f6ecd87511171568a7c1d
Hiepdepzainhat <hiepdepzai2k3@gmail.com>
2023-10-04 23:17:17 +0700

    hiep them file AllResponsitorie

---
+++ SD85_WebBookOnline.Api/Responsitories/AllResponsitories.cs
@@ -0,0 +1,67 @@
+﻿using SD85_WebBookOnline.Api.IResponsitories;
+using SD85_WebBookOnline.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SD85_WebBookOnline.Responsitories
+{
+	public class AllResponsitories<T> : IAllResponsitories<T> where T : class
+	{
+	    private	AppDbContext context;
+		private DbSet<T> dbset;
+        public AllResponsitories()
+        {
+            
+        }
+        public AllResponsitories(AppDbContext context, DbSet<T> dbset)
+        {
+            this.context = context;
+			this.dbset = dbset;	
+        }
+        public async Task<bool> CreateItem(T item)
+		{
+			try
+			{
+				await dbset.AddAsync(item);
+				await context.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> DeleteItem(T item)
+		{
+			try
+			{
+				dbset.Remove(item);
+				await context.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+
+		public async Task<IEnumerable<T>> GetAll()
+		{
+			return await dbset.ToListAsync();
+		}
+
+		public async Task<bool> UpdateItem(T item)
+		{
+			try
+			{
+				dbset.Update(item);
+				await context.SaveChangesAsync();
+				return true;
+			}
+			catch (Exception)
+			{
+				return false;
+			}
+		}
+	}
+}
