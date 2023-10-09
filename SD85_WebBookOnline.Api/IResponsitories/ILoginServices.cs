79d3fd0bf59e1a1c90048fd13bbeab3251eb0617
Toannk <toannkph27888@fpt.edu.vn>
2023-10-09 19:41:49 +0700

    login and register

---
+++ SD85_WebBookOnline.Api/IResponsitories/ILoginServices.cs
@@ -0,0 +1,9 @@
+﻿using SD85_WebBookOnline.Share.ViewModels;
+
+namespace SD85_WebBookOnline.Api.IResponsitories
+{
+    public interface ILoginServices
+    {
+        Task<Response> LoginAsync(LoginUser loginUser);
+    }
+}
---
+++ SD85_WebBookOnline.Share/ViewModels/LoginUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SD85_WebBookOnline.Share.ViewModels
+{
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "Username cannot be blank")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password cannot be blank")]
+        public string Password { get; set; }
+    }
+}
