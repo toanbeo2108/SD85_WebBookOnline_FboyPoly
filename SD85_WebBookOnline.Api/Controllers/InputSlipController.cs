cba8b4cad7ffe533446de8082bc3fae53d3f2214
toanbeo2108 <142005132+toanbeo2108@users.noreply.github.com>
2024-01-12 08:54:26 +0700

    Revert "Vũ Dình Hứng"

--- SD85_WebBookOnline.Api/Controllers/InputSlipController.cs
+++ SD85_WebBookOnline.Api/Controllers/InputSlipController.cs
@@ -26,42 +26,35 @@ namespace SD85_WebBookOnline.Api.Controllers
         [HttpPost("[Action]")]
         public async Task<bool> CreateInputSlip(Guid? idNhanVienNhap, Guid? idSachNhap, int soLuong, DateTime ngayNhap,decimal giaNhap)
         {
+            //var lstauthor = await _irespon.GetAll();
+            //var insp = lstauthor.FirstOrDefault(x => x.AuthorName == authorName);
+            //if (insp != null)
+            //{
+            //    return false;
+            //}
             InputSlip insp = new InputSlip();
-            var book = await _context.Book.FindAsync(idSachNhap);
-            if (book == null)
-            {
-                return false;
-
-            }
-            else
-            {
-
-                insp.InputSlipID = Guid.NewGuid();
-                insp.IdNhanVienNhap = idNhanVienNhap;
-                insp.IdSachNhap = idSachNhap;
-                insp.SoLuong = soLuong;
-                insp.NgayNhap = ngayNhap;
-                insp.GiaNhap = giaNhap;
-                book.QuantityExists += insp.SoLuong  ?? 0;
-                book.TotalQuantity += insp.SoLuong  ?? 0;
-            return await _irespon.CreateItem(insp);
-            }
-        }
-
-        [HttpPut("[Action]/{id}")]
-        public async Task<bool> UpdateInputSlip(Guid id, Guid? idNhanVienNhap, Guid? idSachNhap, int soLuong, DateTime ngayNhap, decimal giaNhap)
-        {
-            var lstauthor = await _irespon.GetAll();
-            var insp = lstauthor.FirstOrDefault(x => x.InputSlipID == id);
-            if (insp == null)
-            {
-                return false;
-            }
-          //  insp.IdNhanVienNhap = idNhanVienNhap;
+            insp.InputSlipID = Guid.NewGuid();
+            insp.IdNhanVienNhap = idNhanVienNhap;
             insp.IdSachNhap = idSachNhap;
             insp.SoLuong = soLuong;
             insp.NgayNhap = ngayNhap;
             insp.GiaNhap = giaNhap;
+            return await _irespon.CreateItem(insp);
+        }
+
+        [HttpDelete("[Action]/{id}")]
+        public async Task<bool> UpdateInputSlip(Guid id, Guid? idNhanVienNhap, Guid? idSachNhap, int soLuong, DateTime ngayNhap)
+        {
+            var lstauthor = await _irespon.GetAll();
+            var insp = lstauthor.FirstOrDefault(x => x.InputSlipID == id);
+            if (insp != null)
+            {
+                return false;
+            }
+            insp.IdNhanVienNhap = idNhanVienNhap;
+            insp.IdSachNhap = idSachNhap;
+            insp.SoLuong = soLuong;
+            insp.NgayNhap = ngayNhap;
 
             return await _irespon.CreateItem(insp);
         }
